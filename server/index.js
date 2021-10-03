82136e4a4697c4ff8602ca87b6cfc25c9ae6231c
Andriw Tapanes <andriwtapanes@gmail.com>
2021-10-03 14:54:09 -0400

    client and server created, socket io implementation started

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const socketio = require('socket.io')
+const http = require('http');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3001
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
+
+  socket.on('join', ({name, room}, callback) => {
+    console.log(name, room);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User has left!");
+  })
+})
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
